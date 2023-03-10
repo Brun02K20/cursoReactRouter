851582efd9483973f147d1acc7aa3e2da699d598
Brun02K23 <bvirinni@gmail.com>
2023-03-10 14:29:22 -0300

    Usando rutas publicas y privadas

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../../auth";
 
 // Notas de clase 7: Link debemos importarlo de react-router-dom; la propiedad to es el equivalente al href de las etiquetas a; Link sabe el tipo de router que estamos usando (browser, hash, etc), y añade o no lo correspondiente al tipo de router (ej: el hash hay que añadirle en cada ruta un #/). 
 
 function Menu () {
+    const auth = useAuth();
+
+    // if(!auth.user) no se renderizan las rutas privadas
+
     return(
         <nav>
             <ul>
-                {routes.map(route => (
-                    <li key={route.to}>
-                        <NavLink 
-                            to={route.to}
-                            end
-                            style={ ({ isActive }) => ({
-                                color: isActive ? 'green' : 'red'
-                            })}
-                        >
-                            {route.text}
-                        </NavLink>
-                    </li>
-                ))}
+                {routes.map(route => {
+                    // solucion del profe
+                    if(route.publicOnly && auth.user) return null
+
+
+                    if(route.private && !auth.user) return null
+
+                    // mi solucion al reto de que se oculte el login cuando ya el usuario hizo login: 
+                    // if (route.to === "/login" && auth.user) return null
+                    return (
+                        <li key={route.to}>
+                            <NavLink 
+                                to={route.to}
+                                end
+                                style={ ({ isActive }) => ({
+                                    color: isActive ? 'green' : 'red'
+                                })}
+                            >
+                                {route.text}
+                            </NavLink>
+                        </li>
+                    );
+
+                })}
             </ul>
         </nav>
     );
@@ -30,26 +46,32 @@ const routes = [];
 routes.push({
     to: "/",
     text: "Home",
+    private: false,
 });
 
 routes.push({
     to: "/blog",
-    text: "Blog"
+    text: "Blog",
+    private: false,
 });
 
 routes.push({
     to: "/profile",
-    text: "Profile"
+    text: "Profile",
+    private: true,
 });
 
 routes.push({
     to: "/login",
-    text: "LOGIN"
+    text: "LOGIN",
+    private: false,
+    publicOnly: true,
 });
 
 routes.push({
     to: "/logout",
-    text: "LOGOUT"
+    text: "LOGOUT",
+    private: true,
 });
 
 
